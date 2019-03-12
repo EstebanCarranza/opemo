9c512dc4b30a46fc072e3d78b00f2c153412b657
EstebanCarranza <esteban.carranza@outlook.com>
2019-03-12 14:39:48 -0600

    correccion publicacion, reporte de usuario

--- resources/views/pages/publication.blade.php
+++ resources/views/pages/publication.blade.php
@@ -7,8 +7,9 @@
 <div class="row card-panel">
     <div class="col l6">
     <img class="col l12 s12 materialboxed" data-caption='{{$title}}' src="http://wallpapers.ae/wp-content/uploads/2015/01/Pier-Night-HD-Picture.jpg">
-     <div class="col s12 m8 offset-m2 l12">
-        <div class="card-panel grey lighten-5 z-depth-1">
+     <div class="col s12 l12">
+     <a href="/profile">
+        <div class="card-panel z-depth-1 report-size ">
           <div class="row valign-wrapper">
             <div class="col s2">
               <img src="img/profile.png" alt="" class="circle responsive-img"> <!-- notice the "circle" class -->
@@ -27,6 +28,7 @@
             </div>
           </div>
         </div>
+        </a>
       </div>
       </div>
     <div class="col l6 s12 ">
@@ -44,7 +46,13 @@
     <div class="col l6 offset-l6 s12 row">
         <a class="col l12 m12 s12 waves-effect waves-light btn orange" href="/reclam">Reclamar objeto</a>
     </div>
+    <div class="col l6 offset-l6 s12 row">
+        <a class="col l12 m12 s12 waves-effect waves-light btn orange modal-trigger" href="#reportar">Reportar publicación</a>
+    </div>
 </div>
+
+ 
+
 <div class="row card-panel z-depth-1">
     <div class="col s12">
         <div class="card-panel z-depth-0">
@@ -72,6 +80,9 @@
         </div>
     </div>
 </div>
+
+
+
 @for($i = 0; $i < 10; $i++)
 <div class="row card-panel z-depth-1">
     <div class="col s12">
@@ -95,10 +106,28 @@
     </div>
 </div>
 @endfor
-       
+        <!-- Modal Structure -->
+  <div id="reportar" class="modal modal-fixed-footer">
+    <div class="modal-content">
+        <h4>Razón del reporte</h4>
+        <p>Especifica la razón por la cual estás haciendo el reporte, recuerda ser claro y conciso</p>
+        <div class="input-field col s12">
+            <textarea id="txtRazonReporte" class="materialize-textarea" data-length="120"></textarea>
+            <label for="txtRazonReporte">Escribe tu razón de bloqueo</label>
+        </div>
+
+    </div>
+    <div class="modal-footer">
+    <a href="#!" class="modal-close waves-effect waves-green btn-flat">Cerrar ventana</a>
+      <a href="#!" class="modal-close waves-effect waves-green btn-flat">Reportar publicación</a>
+    </div>
+  </div>
+
  <script>
 $(document).ready(function(){
     $('.materialboxed').materialbox();
+    $('.modal').modal();
+    $('input#input_text, textarea#txtRazonReporte').characterCounter();
   });
   </script>
 @stop

2d75ea44dbfc4081a26ad6278a8df00aec3f3933
EstebanCarranza <esteban.carranza@outlook.com>
2019-04-14 12:36:55 -0500

    Update

--- app/Http/Controllers/Ciudad.php
+++ app/Http/Controllers/Ciudad.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+//use App\Ciudad;
 
 class Ciudad extends Controller
 {
@@ -18,13 +19,18 @@ class Ciudad extends Controller
         $table = DB::table($table)->where('areaMetropolitana', true);
         return $table;
     }*/
+    
     public function index()
     {
-        $ciudades = DB::table('tbl_ciudad')->get();
+        $ciudades = DB::table('tbl_ciudad')->select('idCiudad','titulo')
+        ->where('areaMetropolitana',true)->get();
         foreach($ciudades as $ciudad)
         {
-            echo $ciudad->titulo . "<br>";
+            echo $ciudad->idCiudad . " - " . $ciudad->titulo . "<br>";
+
         }
+
+        //$ciudadM = new Ciudad();
             
     }
 
